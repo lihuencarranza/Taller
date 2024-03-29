68e9be6d8a4011dba59f019ae00f197ad0e2135a
Lihuén <86395729+lihuencarranza@users.noreply.github.com>
2024-03-29 03:22:20 +0000

    simple char chars work

--- tp1/src/regex.rs
+++ tp1/src/regex.rs
@@ -1,10 +1,17 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum MetacharClass {
     Period,
     None
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
+pub enum State {
+    InProgress,
+    Completed,
+    NotStarted,
+}
+
+
 pub struct Regex {
     literal: Vec<char>,
     metachar: Vec<MetacharClass>,
@@ -27,6 +34,8 @@ impl Regex {
             };
             metachar.push(metachar_class);
         }
+
+
         Ok(Regex {
             literal,
             metachar,
@@ -48,29 +57,45 @@ pub fn check_regex_in_list(regex: Regex, list: &Vec<String>) -> Vec<String>{
     list_coincidences
 }
 
-fn check_word_with_regex(word_iter: &mut std::str::Chars, regex_literal_iter: &mut std::slice::Iter<char>, metachar_iter: &mut std::slice::Iter<MetacharClass>) -> bool{
+fn check_word_with_regex(word_iter: &mut std::str::Chars, mut regex_literal_iter: &mut std::slice::Iter<char>, metachar_iter: &mut std::slice::Iter<MetacharClass>) -> bool{
 
-    let regex_size = regex_literal_iter.clone().count();
+    let regex_size = check_lenght_word_with_regex(word_iter, regex_literal_iter);
+    if regex_size == -1 {
+        return false;
+    } 
 
-    for mut i in 0..regex_size{
+    
+    let regex_literal_iter_initial = regex_literal_iter.clone();
+    //variable si empezó a matchear
+    let mut match_started: State = State::NotStarted;
+
+    while (match_started != State::Completed){
         
-        let char_word = word_iter.next();
-        let char_regex = regex_literal_iter.next();
+        let mut char_word = word_iter.next();
+        let mut char_regex = regex_literal_iter.next();
         
         //si la palabra no tiene mas caracteres pero la expresion regular si
         if char_word.is_none() && char_regex.is_some(){
             return false;
         }
         //si la expresion regular no tiene más caracteres devuelve true
-        if char_regex.is_none(){
-            return true;
+        if char_regex.is_none() && match_started == State::InProgress{
+            match_started = State::Completed;
+            break;
+        }else if char_regex.is_none() && match_started != State::InProgress { 
+            break;
         }
-        // si el char de la palabra coincide con el de la regex, continua
-        if char_word != char_regex.copied(){
-            return false;
+        // si el char de la palabra coincide con el de la regex, avanza el iterador de la palabra y la regex
+        if char_word == char_regex.copied(){
+            match_started = State::InProgress;
+            continue;
+        }else{
+            match_started = State::NotStarted;
+            *regex_literal_iter = regex_literal_iter_initial.clone();
         }
-
-    }
+    } 
+    
+    return match_started == State::Completed;
 
     /*{
         
@@ -89,7 +114,16 @@ fn check_word_with_regex(word_iter: &mut std::str::Chars, regex_literal_iter: &m
         
         
     }*/
-    true
+}
+
+fn check_lenght_word_with_regex(word_iter: &mut std::str::Chars, regex_literal_iter: &mut std::slice::Iter<char>)-> i32{
+    let regex_size = regex_literal_iter.clone().count();
+    //si la expresion es mas larga que la palabra devuelve false
+    let word_size = word_iter.clone().count();
+    if regex_size > word_size{
+        return -1;
+    }
+    regex_size as i32
 }
 
 #[cfg(test)]
@@ -101,37 +135,40 @@ mod tests {
         let expression = "a";
         let regex = Regex::new(expression).unwrap();
         assert_eq!(regex.literal, vec!['a']);
-    }
-
-    #[test]
-    fn test_regex_incorrect_new() {
-        let regex = Regex::new("");
-        match regex {
-            Ok(_) => {
-                panic!("La expresión regular no debería ser válida");
-            }
-            Err(e) => {
-                assert_eq!(e, "Empty expression");
-            }
-        }
-    }
-
-    #[test]
-    fn test_regex_correct_check_regex_in_list() {
-        let expression = "a";
-        let regex = Regex::new(expression).unwrap();
-        let list = vec!["a".to_string(), "b".to_string()];
-        let list_coincidences = check_regex_in_list(regex, &list);
-        assert_eq!(list_coincidences, vec!["a".to_string()]);
-    }
-
-    #[test]
-    fn test_regex_incorrect_check_regex_in_list() {
-        let expression = "a";
-        let regex = Regex::new(expression).unwrap();
-        let list = vec!["b".to_string()];
-        let list_coincidences: Vec<String> = check_regex_in_list(regex, &list);
-
-        //assert_eq!(list_coincidences, vec![]);
+        assert_eq!(regex.metachar, vec![MetacharClass::None]);
+        //assert_eq!(regex.state, State::NotStarted);
     }
 }
+
+#[test]
+fn test_regex_incorrect_new() {
+    let regex = Regex::new("");
+    match regex {
+        Ok(_) => {
+            panic!("La expresión regular no debería ser válida");
+        }
+        Err(e) => {
+            assert_eq!(e, "Empty expression");
+        }
+    }
+}
+
+#[test]
+fn test_regex_correct_check_regex_in_list() {
+    let expression = "a";
+    let regex = Regex::new(expression).unwrap();
+    let list = vec!["a".to_string(), "b".to_string()];
+    let list_coincidences = check_regex_in_list(regex, &list);
+    assert_eq!(list_coincidences, vec!["a".to_string()]);
+}
+
+#[test]
+fn test_regex_incorrect_check_regex_in_list() {
+    let expression = "a";
+    let regex = Regex::new(expression).unwrap();
+    let list = vec!["b".to_string()];
+    //let list_coincidences: Vec<String> = check_regex_in_list(regex, &list);
+
+    //assert_eq!(list_coincidences, vec![]);
+}
+
